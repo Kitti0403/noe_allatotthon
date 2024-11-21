312682a72716ad2a20094768a7f342443e9e89f7
Csilluslol <106381397+Csilluslol@users.noreply.github.com>
2024-11-21 11:13:43 +0100

    navbar
    oldalak létrehozása, navbar js-ben

---
+++ script.js
@@ -0,0 +1,35 @@
+document.getElementById("navbar").innerHTML =
+` 
+<div class="header">
+        <div class="nav">
+            <div class="logo">
+            [logo]
+            </div>
+            
+            <a href="../about_us/rolunk.html"><button> 
+                Rólunk
+            </button></a>
+
+            <a href="../elerhetosegek/elerhetoseg.html"><button>
+                Elérhetőségeink
+            </button></a>
+
+
+            <a href="../adomanyozas/adomany.html"><button>
+                Adományozás
+            </button></a>
+
+            <!-- burger menu -->
+            
+            <div class="icons">
+                <i class="fa fa-home">
+                </i>
+                <i class="fa fa-bars">
+                </i>
+                <i class="fa fa-paw">
+                </i>
+                <!-- --------------- -->
+            </div>
+        </div>
+    </div>
+`;
